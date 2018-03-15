1d40c39e6d68c373def6d6265904bc9a19e8c4fa
Logan <lbarkes@ufl.edu>
2018-03-15 13:35:41 -0400

    Added Camera Zones and Switches zones based on ai location

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+    public void CutToShot () {
+        Camera.main.transform.localPosition = this.transform.position;
+        Camera.main.transform.localRotation = this.transform.rotation;
+    }
+}
